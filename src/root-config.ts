c7d997de2b6aa1f0b6da0c2587b45ebcd125225b
Aaqibhafeezkhan <aaqibhafeezkhan@gmail.com>
2026-04-15 21:02:49 +0530

    feat: implement application routing via single-spa-layout

--- src/root-config.ts
+++ src/root-config.ts
@@ -1,16 +1,16 @@
 export default `
   <single-spa-router>
+    <route path="auth">
+      <application name="cms-angular-auth"></application>
+    </route>
     <route path="editorial">
       <application name="cms-react-editorial"></application>
     </route>
-    <route path="media-library">
-      <application name="cms-vue-media"></application>
-    </route>
-    <route path="role-access">
-      <application name="cms-angular-auth"></application>
-    </route>
-    <route path="real-time-editor">
+    <route path="collab">
       <application name="cms-svelte-collab"></application>
+    </route>
+    <route path="media">
+      <application name="cms-vue-media"></application>
     </route>
   </single-spa-router>
 `;
