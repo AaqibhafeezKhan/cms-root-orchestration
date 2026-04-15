de7b8eb75b6e82676f2cf67277598add7849d402
Aaqibhafeezkhan <aaqibhafeezkhan@gmail.com>
2026-04-15 21:56:38 +0530

    fix: set libraryTarget to system in webpack to resolve single is not defined error

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/cms-root-orchestration/',
+    libraryTarget: 'system',
   },
   resolve: {
     extensions: ['.ts', '.js'],
